d7bd4cfd997b86917fa18a4d3e70b4626cb3f3b0
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-01-25 22:42:05 +0100

    adds userName in object array

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const userPage = document.getElementById("userPage");
 //************************ btLogIn  ************************
 const btnLogIn = document.getElementById("btnLogIn");
 // console.log(btnLogIn);
+
+
+
 btnLogIn.addEventListener("click", function (){
     console.log("klick btnLogIn");
     main.innerHTML = `        
@@ -23,7 +26,36 @@ btnLogIn.addEventListener("click", function (){
         <button id="btnSend">Skicka</button>
         <div id="errorMess">Error</div>
     </section>`;
+
+    btnSend.addEventListener("click", function () {
+        const inputField = document.getElementById("inputField");
+        const btnSend = document.getElementById("btnSend");
+
+        // console.log(inputField.value);
+        let inputUserName = inputField.value;
+
+        let users = [
+            {"userName": "Janne"},
+            {"userName": inputUserName}
+        ];
+        console.log(users);
+
+        
+    });
 });
+
+
+
+
+
+
+
+//Grunden
+//1) localStorage.setItem("userName", "Janne");
+
+//2)Kunna se Janne i loggen
+//2a) let userName = localStorage.getItem("userName", "Janne");
+//2b) console.log(userName);
 
 
 
