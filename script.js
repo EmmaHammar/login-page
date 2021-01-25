aeae230edc5c6627c34d38772f6bba01fff6b244
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-01-25 11:10:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//koppling DOMen och variabler
+const header = document.getElementById("header");
+//dynamiskt kunna byta ut alt lägga till/ta bort li i menyn
+//dynamiskt kunna byta ut h2 och p i main
+
+//När du trycker på logga in-knappen i menyfältet syns: 
+    //logga in-vy: inputfält, knapp, plats för error. Göra en container som syns i vissa läge? 
+    //När du trycker på logga in-knappen i main: testar den om användarnamnet/lösen finns -> ja: inloggad vy, nej: error
+
+
+//Inloggad vy: 
+    //meny: logga ut syns (istället för logga in)
+    //h2 och p ändrat
+    
+
+
+
+
+
+
+
+//NÄR KLAR MED OVAN
+//(ev första gången du logga in - fält)
+//i inloggad vy ser du ex Hej Emma. 
+
