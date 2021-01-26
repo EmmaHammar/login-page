8505274260120a2a8c6447a58f88c22805acfb65
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-01-26 22:02:17 +0100

    fix error when logged in

--- script.js
+++ script.js
@@ -11,14 +11,16 @@ if (localStorage.getItem("userName") === null) {
 };
 
 let userList = [ 
-    {userName: "janne", password: "test"},
-    {userName: "emma", password: "test2"},
-    // {userName: newUserName, password: newPassword}
+    {userName: "janne", password: "test"}
+    // {userName: "emma", password: "test2"}
 ];
+
+let userNames = userList[0].userName; 
+let userPasswords = userList[0].password
 
 function printStartPage() {
 main.innerHTML = `        
-    <section class="loginPage">
+    <section>
         <h2>Logga in</h2> 
         <p>Användarnamn<p>
         <input id="inputUserName" type="text" placeholder="Användarnamn"> 
@@ -26,10 +28,6 @@ main.innerHTML = `
         <input id="inputPassword" type="text" placeholder="Lösenord"> 
         <button id="btnSend">Skicka</button>
         <div id="errorMess"></div>
-        <br>
-        <br>
-        <p>Ny användare?</p>
-        <button id="btnAccount">Skapa konto</button>
     </section>`;
 
     let rightPassword = false; // finns ej innan vi har börjat leta
@@ -42,13 +40,10 @@ main.innerHTML = `
         const btnSend = document.getElementById("btnSend");
         
         for (i=0; i < userList.length; i++) {
-            console.log(userList[i].userName);
-            console.log(userList[i].password);
+            console.log(userList[0].userName);
+            console.log(userList[0].password);
 
-            let userNames = userList[i].userName; 
-            let userPasswords = userList[i].password;
-
-            if (inputUserName == userList[i].userName && inputPassword == userList[i].password) {
+            if (inputUserName == userList[0].userName && inputPassword == userList[0].password) {
                 rightPassword = true;
                 console.log("visa inloggade vyn");
                 localStorage.setItem("userName", JSON.stringify(userNames));
@@ -59,24 +54,17 @@ main.innerHTML = `
             };
         };
     });
-
-    const btnAccount = document.getElementById("btnAccount");
-    btnAccount.addEventListener("click", function () {
-        console.log("klick skapa konto");
-        printCreateAccountPage();
-    });
-
 };
 
 function printUserPage() {
     main.innerHTML = `
-    <section id="userPage">
-        <h2>Välkommen ${localStorage.getItem("userName")} till din personliga sida!</h2> 
+    <section>
+        <h2>Välkommen till din personliga sida!</h2> 
         <p>Här kan du se dina grejer.</p>
         <button id="btnLogOut">Logga ut</button>
     </section>`;
 
-    const btnLogOut = document.getElementById("btnLogOut");
+    let btnLogOut = document.getElementById("btnLogOut");
     btnLogOut.addEventListener("click", function () {
         console.log("klick logga ut");
         localStorage.clear(); 
@@ -84,36 +72,3 @@ function printUserPage() {
         printStartPage();
     });
 };
-
-function printCreateAccountPage() {
-    main.insertAdjacentHTML("beforeend", `
-        <section class="signUpPage">
-            <h2>Ny användare</h2> 
-            <input id="inputUserNameNew" type="text" placeholder="Välj användarnamn"> 
-            <p>Lösenord</p>
-            <input id="inputPasswordNew" type="text" placeholder="Välj lösenord"> 
-            <button id="btnCreateNewUser">Nytt konto</button>
-        </section>`);
-
-    const btnCreateNewUser = document.getElementById("btnCreateNewUser");
-    btnCreateNewUser.addEventListener("click", function () {
-        console.log("klick skicka formuläret");
-        let newUserName = document.getElementById("inputUserNameNew").value;
-        console.log(newUserName);
-        let newPassword = document.getElementById("inputPasswordNew").value;
-        console.log(newPassword);
-
-        let addToUserList = [
-            {userName: newUserName, password: newPassword}
-        ];
-        console.log(addToUserList);
-        userList.push(addToUserList);
-        console.log(userList); //uppdated array with new userName. Denna vill jag ska in i 44:an - HUR???
-
-        //hämta logga in-funktionen + lägg till nya arrayen. HUR?
-    });
-
-};
-
-//Frågor
-//Får felmeddelande när jag loggar in med janne men ej Emma. Uncaught TypeError: Cannot set property 'innerHTML' of null at HTMLButtonElement.<anonymous> (script.js:58)
