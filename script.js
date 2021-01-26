8915f25a3aa6764b7ca686fbef61fdb7df0060bd
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-01-26 14:18:41 +0100

    cleaning the index and script files from internal comments overload

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-//koppling DOMen och variabler
-const header = document.getElementsByTagName("HEADER")[0];
-const ul = document.createElement("UL");
-header.appendChild(ul);
-
 const main = document.getElementsByTagName("MAIN")[0];
 main.id = "main";
 const startPage = document.getElementById("startPage");
 const loginPage = document.getElementById("loginPage");
 const userPage = document.getElementById("userPage");
 
-//************************ HEADER - btLogIn **********************
-const li = document.createElement("LI");
-ul.appendChild(li);
-// li.insertAdjacentHTML("afterbegin", `<button id="btnLogIn">Logga in</button>`)
-// console.log(ul);
-
-//************************ Janne to local storage  
-// let userNames = ["janne"];
-// // console.log(userNames[0]);
-// localStorage.setItem("userName", userNames[0]);
+//print welcome page om ngn är inloggad, default | print userPage om ngn är inloggad (i localStorage)
+if (localStorage.getItem("userName") === null) {
+    console.log("ingen är inloggad");
+    printStartPage();
+} else {
+    console.log("någon är inloggad");
+    printUserPage();
+};
 
 let userList = [ 
     {userName: "janne", password: "test"}
@@ -27,9 +20,6 @@ let userList = [
 
 let userNames = userList[0].userName; 
 let userPasswords = userList[0].password
-
-//************************ check if userName exists  1/2
-// let isLoggedIn = false; //utloggad vy
 
 function printStartPage() {
 main.innerHTML = `        
@@ -50,10 +40,8 @@ main.innerHTML = `
         // console.log(inputUserName);
         let inputPassword = document.getElementById("inputPassword").value;
         // console.log(inputPassword);
-
         const btnSend = document.getElementById("btnSend");
         
-        //************************ check if userName+password is correct  2/2
         for (i=0; i < userList.length; i++) {
             console.log(userList[0].userName);
             console.log(userList[0].password);
@@ -63,8 +51,7 @@ main.innerHTML = `
                 console.log("visa inloggade vyn");
                 localStorage.setItem("userName", JSON.stringify(userNames));
                 printUserPage();
-            }
-            else {
+            } else {
                 let errorMess = document.getElementById("errorMess");
                 errorMess.innerHTML = "Error, vänligen kontrollera så att du skrivit rätt användarnamn och lösenord.";
             };
@@ -79,6 +66,7 @@ function printUserPage() {
         <p>Här kan du se dina grejer.</p>
         <button id="btnLogOut">Logga ut</button>
     </section>`;
+
     let btnLogOut = document.getElementById("btnLogOut");
     btnLogOut.addEventListener("click", function () {
         console.log("klick logga ut");
@@ -86,16 +74,4 @@ function printUserPage() {
         location.reload(); //laddar om sidan
         printStartPage();
     });
-};
-
-//print welcome page om ngn är inloggad, default
-//print userPage om ngn är inloggad (i localStorage)
-if (localStorage.getItem("userName") === null) {
-    console.log("ingen är inloggad");
-    printStartPage();
-
-}
-else {
-    console.log("någon är inloggad");
-    printUserPage();
 };
